c36c108ff0fc0af5c658706a31157f0115028888
HongWei Hsu <hongweih@student.unimelb.edu.au>
2023-10-04 23:07:04 +1100

    set nav bar

--- src/components/NavigationBar/NavigationBar.jsx
+++ src/components/NavigationBar/NavigationBar.jsx
@@ -1,7 +1,48 @@
 import React from 'react';
+import { AppBar, IconButton, Toolbar, Drawer, Button, Avatar, useMediaQuery } from '@mui/material';
+import { Menu, AccountCircle, Brightness4, Brightness7 } from '@mui/icons-material';
+import { Link } from 'react-router-dom';
+import { useTheme } from '@mui/material/styles';
 
-const NavigationBar = () => (
-  <div>NavigationBar</div>
-);
+import useStyles from './styles';
+
+const NavigationBar = () => {
+  const classes = useStyles();
+  const theme = useTheme();
+  const isMobile = useMediaQuery('(max-width:600px)');
+  const isAuthenticated = true;
+
+  return (
+    <>
+      <AppBar position="fixed">
+        <Toolbar className={classes.toolbar}>
+          {isMobile && (
+            <IconButton className={classes.menuButton} color="inherit" edge="start" style={{ outline: 'none' }} onClick={() => {}}>
+              <Menu />
+            </IconButton>
+          )}
+          <IconButton color="inherit" sx={{ ml: 1 }} onClick={() => { ''; }}>
+            {theme.palette.mode === 'dark' ? <Brightness7 /> : <Brightness4 />}
+          </IconButton>
+          {!isMobile && 'Search ... '}
+          <div>
+            {!isAuthenticated ? (
+              <Button color="inhirit" onClick={() => {}}>
+                Login $nbsp; <AccountCircle />
+              </Button>
+            ) : (
+              <Button className={classes.linkButton} color="inherit" component={Link} to="/profile/:id" onClick={() => {}}>
+                {!isMobile && <>My Movies $nbsp;</>}
+                <Avatar style={{ width: 30, height: 30 }} alt="Profile" src="https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png" />
+              </Button>
+            )}
+          </div>
+          {isMobile && 'Search ... '}
+        </Toolbar>
+      </AppBar>
+    </>
+
+  );
+};
 
 export default NavigationBar;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
-// eslint-disable-next-line
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import App from './components/App';
 
-// createRoot(document.getElementById('root')).render(<App />);
+const theme = createTheme({});
+
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <ThemeProvider theme={theme}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ThemeProvider>,
   document.getElementById('root'),
 );
